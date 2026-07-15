17f4ef88c0f787e82cd175c20612378e812b13f5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-07-15 12:13:04 -0500

    Implement typeSlowly function for input simulation
    Added a function to simulate slow typing in an input element.

--- react-set.js
+++ react-set.js
@@ -22,3 +22,21 @@ const reactSet = (el,value)=>{
 const input = document.querySelector('input[data-testid="search-input"]');
 
 reactSet(input,'asfd');
+
+
+async function typeSlowly(el, value) {
+    el.focus();
+    el.value = "";
+    el.dispatchEvent(new Event("input", { bubbles: true }));
+
+    for (const char of value) {
+      el.value += char;
+      el.dispatchEvent(new Event("input", { bubbles: true }));
+      await new Promise((r) => setTimeout(r, 1));
+    }
+
+    el.dispatchEvent(new Event("change", { bubbles: true }));
+    el.blur();
+  }
+  
+  typeSlowly(document.querySelector('[data-slate-string="true"]'),'ASDF')
